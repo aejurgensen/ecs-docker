7a171d76a48eb7384170328e134e1f473cd5c1fc
aejurgensen <aejurgensen@dons.usfca.edu>
2021-05-13 11:52:19 -0700

    added unit tests

--- src/capitals_unit_tests.py
+++ src/capitals_unit_tests.py
@@ -17,3 +17,44 @@ def test_match_search_abbrev():
     actual = caps.match_search_string("new m")
     assert expected == actual
 
+
+def test_get_name():
+    expected = "New Jersey (NJ)"
+    actual = caps.get_name(caps.get_state("NJ"))
+    assert expected == actual
+
+
+def test_get_capital():
+    expected = "Denver"
+    actual = caps.get_capital(caps.get_state("Colorado"))
+    assert expected == actual
+
+
+def test_get_statehood_state():
+    expected = "gained statehood in 1959"
+    actual = caps.get_statehood(caps.get_state("Alaska"))
+    assert expected == actual
+
+
+def test_get_statehood_territory():
+    expected = "US territory"
+    actual = caps.get_statehood(caps.get_state("Guam"))
+    assert expected == actual
+
+
+def test_get_statehood_obsolete():
+    expected = "obsolete entity"
+    actual = caps.get_statehood(caps.get_state("Orleans"))
+    assert expected == actual
+
+
+def test_get_timezones_single():
+    expected = "America/Los_Angeles"
+    actual = caps.get_timezones(caps.get_state("WA"))
+    assert expected == actual
+
+
+def test_get_timezones_multi():
+    expected = "America/Chicago, America/Denver"
+    actual = caps.get_timezones(caps.get_state("Kansas"))
+    assert expected == actual
