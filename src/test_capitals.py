4654a88dd1a3eeeb59c5bcff8cfe70e986cc5db9
aejurgensen <aejurgensen@dons.usfca.edu>
2021-05-15 12:10:47 -0700

    added additional tests

--- src/test_capitals.py
+++ src/test_capitals.py
@@ -58,3 +58,25 @@ def test_get_timezones_multi():
     expected = "America/Chicago, America/Denver"
     actual = caps.get_timezones(caps.get_state("Kansas"))
     assert expected == actual
+
+
+def test_get_state_info():
+    expected = {"name":"New Jersey (NJ)",
+               "capital":"Trenton",
+               "statehood":"gained statehood in 1787",
+               "timezones":"America/New_York"}
+    actual = caps.get_state_info("New Jersey")
+    assert expected == actual
+
+
+def test_get_safe_state_info():
+    found = caps.get_safe_state_info("III")
+    if found is None:
+        keys = False
+        values = False
+    else:
+        keys = list(found.keys()) == ['name','capital','statehood','timezones']
+        values_are_str = [type(x)==str for x in found.values()]
+        values = sum(values_are_str) == len(found)
+
+    assert found is not None and keys == True and values == True
